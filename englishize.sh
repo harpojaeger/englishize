2665252fd80ad431fd9e445d785282b391b70406
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-06-12 12:03:49 +0200

    Size calculations fixed
    Also log files to du.out for deletion after scanning.

--- englishize.sh
+++ englishize.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 clear
-sudo find /  -name *.lproj ! -name en*.lproj -print0 | xargs -0 du | awk '
+sudo find /Applications -type d -iname *.lproj ! -iname base.lproj ! -iname en*.lproj -prune -print0 | xargs -0 du | awk '
 BEGIN{
 printf("\033[s")
-Ssuff = "KB_MB_GB_TB"
+Ssuff = "B_KB_MB_GB_TB"
 split(Ssuff, suff, "_")
 }{
 
-sum += $1
+sum += ($1*512)
 count+=1
 i=1
 hsize = sum
 while (hsize > 1024) {
-  hsize=hsize/1024
+  hsize = hsize/1024
   i++
 }
 
-  printf("\033[K%d files, %.2f %s\n\033[K%.60s\n",count,hsize,suff[i],$2)
-    printf("\033[u")
+  printf("\033[K%d folders, %.2f %s\n\033[K%.80s\n",count,hsize,suff[i],$0)
+  printf("\033[u")
+  print $0 > "du.out"
+    
 }
 END{
 printf("\n\n")
