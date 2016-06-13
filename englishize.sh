9fb90e8b509dfd0451a77c678b8ce2ac727d08a7
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-06-13 17:51:09 +0200

    Preview files with less and tag du.out w/ PID

--- englishize.sh
+++ englishize.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 clear
-sudo find /Applications/Cyberduck.app -type d -iname *.lproj ! -iname uk.lproj ! -iname base.lproj ! -iname en*.lproj -prune -print0 | xargs -0 du -s | awk -F "\t" '
+sudo find /Applications/Chicken.app -type d -iname *.lproj ! -iname uk.lproj ! -iname base.lproj ! -iname en*.lproj -prune -print0 | xargs -0 du -s | awk -v p=$$ -F "\t" '
 BEGIN{
-printf("\033[s")
-Ssuff = "B_KB_MB_GB_TB"
-split(Ssuff, suff, "_")
+  printf("\033[s")
+  Ssuff = "B_KB_MB_GB_TB"
+  split(Ssuff, suff, "_")
 }{
-
-sum += ($1*512)
-count+=1
-i=1
-hsize = sum
-while (hsize > 1024) {
-  hsize = hsize/1024
-  i++
-}
-
+  sum += ($1*512)
+  count+=1
+  i=1
+  hsize = sum
+  while (hsize > 1024) {
+    hsize = hsize/1024
+    i++
+  }
   printf("\033[K%d folders, %.2f %s\n\033[K%.80s\n",count,hsize,suff[i],$0)
   printf("\033[u")
-
-  print $2 > "du.out"
-  
-}
-END{
-printf("\n\n")
+  print $2 > sprintf("du-%s.out",p)
 }
 '
-printf "Files slated for deletion now listed in du.out.  Delete now (Y/n)? "
-read action
 
-if [ $action = "Y" ]; then
+if [ -s du-$$.out ]; then
+  printf "\n\n"
+  printf "Preview folders slated for deletion? (Y/n)? "
+  read action
+  
+  if [ $action = "Y" ]; then
+    less du-$$.out
+  fi
 
-awk -v q="'" '{
-print q $0 q
+  printf "Delete folders now (Y/n)? "
+  read action
 
+  if [ $action = "Y" ]; then
+    awk -v q="'" '{print q $0 q }' du-$$.out | xargs sudo rm -rf
+  fi
+  rm du-$$.out
 
-}' du.out | xargs sudo rm -rf
-
+else
+  echo "No folders found."
 fi
